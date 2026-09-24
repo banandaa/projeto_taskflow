9402857e07fa859fd30ad58c4e6feed111173555
senai_26_27 <ananda@edu.senai.br>
2026-09-24 16:36:35 -0300

    initial

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 
 # importar  bibliotecas
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from sqlalchemy.exc import SQLAlchemyError
 
-from models import Pessoa
+from models import Pessoa, db_session
 
 ###################################################################################################
 # criar objeto flask "apelido - app"
@@ -56,13 +57,41 @@ def pessoa():
 
 @app.route('/criar_pessoa', methods=['GET', 'POST'])
 def criar_pessoa():
+    #verifica o metodo, se for get vai para a pagina do formulario
     if request.method == 'GET':
         return render_template('criar_pessoa.html')
+
+
+    #recebe os dados do formulario via POST
     nome_form = request.form.get('form_nome')
     email_form = request.form.get('form_email')
     senha_form = request.form.get('form_senha')
-    print(f'nome: {nome}, email: {email}, senha: {senha}')
-    nova_pessoa = Pessoa(nome=nome_form, email=email_form, senha_hash=senha_form)
+    print(f'nome: {nome_form}, email: {email_form}, senha: {senha_form}')
+    if not nome_form:
+        flash('Preencha o nome', 'error')
+        return render_template('criar_pessoa.html')
+
+    try:
+        #Cria uma nova pessoa e adiciona na base de dados
+        nova_pessoa = Pessoa(nome_pessoa=nome_form, email_pessoa=email_form, senha_pessoa=senha_form)
+
+
+        #inicializa a sessão com o banco de dados
+        db_session.add(nova_pessoa)
+        db_session.commit()
+        print(f'{nova_pessoa}')
+        return render_template('pessoa.html')
+    except SQLAlchemyError as e:
+        db_session.rollback() #reverte a transação em caso de erro
+        print (f'Erro ao salvar pessoa no banco: {e}')
+        flash('Erro ao salvar pessoa no banco', 'error')
+        return render_template('criar_pessoa.html')
+    except Exception as e:
+        db_session.rollback()
+        print(f'Espero inesperado: {e}')
+        flash('Espero inesperado', 'error')
+        return render_template('criar_pessoa.html')
+
 
 ###################################################################################################
 # iniciar aplicação web
--- models.py
+++ models.py
@@ -1,59 +1,61 @@
 # importando biblioteca
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Date
-from sqlalchemy.orm import relationship, sessionmaker,declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Date, func
+from sqlalchemy.orm import relationship, sessionmaker,declarative_base, scoped_session
 
 # Base de dados - endereço
 engine = create_engine('mysql+pymysql://root:senaisp@localhost:3306/taskflow')
 
 #config sessao
 #db é database
-db_session = sessionmaker(bind=engine)
+db_session = scoped_session (sessionmaker(bind=engine))
 
 Base = declarative_base()
 
 class Tarefa(Base):
     __tablename__ = 'tarefa'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(30), nullable=False)
-    data = Column(DateTime, nullable=False)
-    hora = Column(DateTime, nullable=False)
-    recurso = Column(Interer, nullable=False)
-    prioridade = Column(String(20), nullable=False)
-    categoria = Column(String(20), nullable=False)
-    responsavel = Column(String(20), nullable=False)
-    descricao = Column(String(100), nullable=False)
+    nome_tarefa = Column(String(30), nullable=False)
+    data_tarefa = Column(DateTime, nullable=False)
+    hora_tarefa = Column(DateTime, nullable=False)
+    recurso_tarefa = Column(Integer, nullable=False)
+    prioridade_tarefa = Column(String(20), nullable=False)
+    categoria_tarefa = Column(String(20), nullable=False)
+    responsavel_tarefa = Column(String(20), nullable=False)
+    descricao_tarefa = Column(String(100), nullable=False)
     criado_em = Column(DateTime, nullable=False, server_default=func.now())
 
     def __repr__(self):
-        return f'Tarefa {self.nome}, data {self.data}, hora {self.hora}, recurso {self.recurso}, prioridade {self.prioridade}, categoria {self.categoria}, responsavel {self.responsavel}, descricao {self.descricao}'
+        return f'Tarefa {self.nome_tarefa}, data {self.data_tarefa}, hora {self.hora_tarefa}, recurso {self.recurso_tarefa}, prioridade {self.prioridade_tarefa}, categoria {self.categoria_tarefa}, responsavel {self.responsavel_tarefa}, descricao {self.descricao_tarefa}, criado_em{self.criado_em}'
 
 class Pessoa(Base):
     __tablename__ = 'pessoas'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(30), nullable=False)
-    email= Column(String(20), nullable=False, unique=True)
-    senha_hash = Column(String(20), nullable=False)
+    nome_pessoa = Column(String(30), nullable=False)
+    email_pessoa= Column(String(20), nullable=False, unique=True)
+    senha_pessoa = Column(String(20), nullable=False)
     papel = Column(String(20), default='usuario', nullable=False)
     criado_em = Column(DateTime, nullable=False, server_default=func.now())
 
     def __repr__(self):
-        return f'Pessoa {self.nome}, email {self.email}, senha {self.senha_hash}'
+        return f'Pessoa {self.nome_pessoa}, email {self.email_pessoa}, senha {self.senha_pessoa}, criado_em{self.criado_em}'
 
 class Tipo(Base):
     __tablename__ = 'tipos'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(30), nullable=False)
-    descricao = Column(String(30), nullable=False)
+    nome_tipo = Column(String(30), nullable=False)
+    descricao_tipo = Column(String(30), nullable=False)
+    criado_em = Column(DateTime, nullable=False, server_default=func.now())
 
     def __repr__(self):
-        return f'Tipo {self.nome}, descricao {self.descricao}'
+        return f'Tipo {self.nome_tipo}, descricao {self.descricao_tipo}, criado_em{self.criado_em}'
 
 class Recurso(Base):
     __tablename__ = 'recursos'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(30), nullable=False)
-    descricao = Column(String(30), nullable=False)
-    tipo = Column(String(30), nullable=False)
+    nome_recurso = Column(String(30), nullable=False)
+    descricao_recurso = Column(String(30), nullable=False)
+    tipo_recurso = Column(String(30), nullable=False)
+    criado_em = Column(DateTime, nullable=False, server_default=func.now())
 
     def __repr__(self):
-        return f'Recurso {self.nome}, descricao {self.descricao}, tipo {self.tipo}'
+        return f'Recurso {self.nome_recurso}, descricao {self.descricao_recurso}, tipo {self.tipo_recurso}, criado_em{self.criado_em}'
